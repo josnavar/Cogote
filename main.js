78090a5f2e8ef552ffe7a789a41f3b79bae9e2ad
siz <sizfeetunder@github.com>
2018-06-02 02:55:06 -0400

    . . .

--- main.js
+++ main.js
@@ -123,7 +123,7 @@ Util.events(document, {
             }
         }
         async function request_search(e){
-            if (e.keyCode==13 || e.keyCode==115){
+            if (e.keyCode==13 || e.keyCode==20 ){
                 var query=document.getElementById("search_query").value;
 
                 var raw_data=await get_raw_data(query);
